10792dc22b293c019059b0ce611034c9dbe53ed2
Chayka1 <m.chayka.v@gmail.com>
2023-09-23 16:39:27 +0300

    🐛Fix models

--- team/models.py
+++ team/models.py
@@ -1,23 +1,29 @@
+from django.conf import settings
 from django.db import models
-
-from users.models import User
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     admin = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="admin_of_teams"
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="admin_of_teams",
     )
-    worker = models.ManyToManyField(User, related_name="worker_of_teams")
+    worker = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="worker_of_teams"
+    )
 
     class Meta:
-        db_table = "teams"
+        db_table = "team"
 
 
 class Task(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     task = models.TextField()
 
     class Meta:
-        db_table = "tasks"
+        db_table = "task"
