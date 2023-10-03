eb7a64536516b96e9387d04dc5b7e50a9ac35f1c
Chayka1 <m.chayka.v@gmail.com>
2023-10-03 20:58:51 +0300

    Create new api

--- team/api.py
+++ team/api.py
@@ -1,14 +1,23 @@
-from rest_framework.decorators import permission_classes
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Team
+from team.models import Team
+from team.serializers import TeamSerializer
 
 
-@permission_classes([IsAuthenticated])
-def create_team(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        admin = request.user
+class TeamCreateView(CreateAPIView):
+    serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticated]
 
-        team = Team.objects.create(name=name, admin=admin)
-        team.workers.add(request.user)
+    def create(self, request, *args, **kwargs):
+        request.data["admin"] = request.user.id
+        return super().create(request, *args, **kwargs)
+
+
+class TeamAPIView(ListAPIView):
+    serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Team.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        return TeamCreateView.as_view()(request._request).render()
