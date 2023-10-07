b42a03e383850afbb837f9969aa55374f3e1966f
Chayka1 <m.chayka.v@gmail.com>
2023-10-07 16:33:21 +0300

    ✅Adding new logic for adding workers to the team

--- team/api.py
+++ team/api.py
@@ -1,8 +1,11 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from team.models import Team
 from team.serializers import TeamSerializer
+from users.models import User
 
 
 class TeamCreateView(CreateAPIView):
@@ -14,10 +17,37 @@ class TeamCreateView(CreateAPIView):
         return super().create(request, *args, **kwargs)
 
 
-class TeamAPIView(ListAPIView):
+class TeamAPIView(RetrieveUpdateAPIView):
     serializer_class = TeamSerializer
     permission_classes = [IsAuthenticated]
     queryset = Team.objects.all()
 
     def post(self, request, *args, **kwargs):
-        return TeamCreateView.as_view()(request._request).render()
+        name = request.data.get("name")
+        worker_id = request.data.get("worker")
+
+        try:
+            team = Team.objects.get(name=name)
+        except Team.DoesNotExist:
+            return Response(
+                {
+                    "detail": f'Team with specified name "{name}" does not exist.'  # noqa
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        try:
+            worker = User.objects.get(id=worker_id)
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "detail": f'User with specified ID "{worker_id}" does not exist.'  # noqa
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        team.worker = worker
+        team.save()
+
+        serializer = TeamSerializer(team)
+        return Response(serializer.data, status=status.HTTP_200_OK)
