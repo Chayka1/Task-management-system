833fcf2c659113486961ae2fa7333b7f4e864108
Chayka1 <m.chayka.v@gmail.com>
2023-10-08 17:53:43 +0300

    ✨New api.py

--- team/api.py
+++ team/api.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -13,41 +13,25 @@ class TeamCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        request.data["admin"] = request.user.id
-        return super().create(request, *args, **kwargs)
-
-
-class TeamAPIView(RetrieveUpdateAPIView):
-    serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
-    queryset = Team.objects.all()
-
-    def post(self, request, *args, **kwargs):
         name = request.data.get("name")
         worker_id = request.data.get("worker")
+        task = request.data.get("task")
+        admin = request.user
 
-        try:
-            team = Team.objects.get(name=name)
-        except Team.DoesNotExist:
-            return Response(
-                {
-                    "detail": f'Team with specified name "{name}" does not exist.'  # noqa
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
+        team = Team.objects.create(name=name, admin=admin, task=task)
 
-        try:
-            worker = User.objects.get(id=worker_id)
-        except User.DoesNotExist:
-            return Response(
-                {
-                    "detail": f'User with specified ID "{worker_id}" does not exist.'  # noqa
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
-
-        team.worker = worker
-        team.save()
+        if worker_id:
+            try:
+                worker = User.objects.get(id=worker_id)
+                team.worker = worker
+                team.save()
+            except User.DoesNotExist:
+                return Response(
+                    {
+                        "detail": f'User with specified ID "{worker_id}" does not exist.'  # noqa
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
 
         serializer = TeamSerializer(team)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
